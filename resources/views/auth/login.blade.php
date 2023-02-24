07f69f3a42530fc986d8ad7ef840c5827b17bad6
rosmery <chcrosmery@gmail.com>
2023-02-24 10:17:06 -0400

    se quito el sibelis del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="divlogin1">
             <div class="card">
-                <div class="card-header empresa">{{ __('SIBELIS') }}</div>
+                <div class="card-header empresa">{{ __('DEMO') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
